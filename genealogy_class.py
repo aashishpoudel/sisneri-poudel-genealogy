4558c66ae63d26e0b31486e2ce4a9fd82f78a397
Aashish Poudel <aashishpoudel@aashishs-imac.lan>
2025-07-21 14:46:15 -0700

    poudel genealogy data

--- genealogy_class.py
+++ genealogy_class.py
@@ -29,30 +29,3 @@ class Person:
 
     def __repr__(self):
         return f"Person({self.name}, {self.birth_year}, {len(self.children)} children)"
-
-# gopal = Person("Gopal", 1940)
-# # father = Person("Mike", 1970)
-# # child = Person("Tom", 2000)
-#
-# ram_bhadra = Person("Ram Bhadra", None)
-# gopal.add_child(ram_bhadra)
-#
-# govinda = Person("Govinda", None)
-# ram_bhadra.add_child(govinda)
-#
-# prajapati = Person("Prajapati", None)
-# ram_bhadra.add_child(prajapati)
-#
-# chamu = Person("Chamu", None)
-# chamu.add_child(chamu)
-#
-# # Save to file
-# with open("genealogy_tree.json", "w") as f:
-#     json.dump(gopal.to_dict(), f, indent=2)
-#
-# def print_tree(person, level=0):
-#     print("  " * level + f"{person.name} ({person.birth_year})")
-#     for child in person.children:
-#         print_tree(child, level + 1)
-#
-# print_tree(gopal)
