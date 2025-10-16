b0d745427806b2da3d4c41370dedf87397bec35a
Aashish Poudel <aashishpoudel@Aashishs-MacBook-Air.local>
2025-10-16 13:08:50 -0700

    genealogy timeline

---
+++ genealogy_constants.py
@@ -0,0 +1,16 @@
+
+GENERATION_COLORS = ['red', 'green', 'blue', 'orange', 'purple', 'teal', 'brown', '#C71585', 'navy', 'darkmagenta']
+TIMELINE_DATA = {
+    "timeline_nep":[
+        ("ई.पू ६००", "आत्रेय"),
+        ("ई.पू १५०", "चरक"),
+        ("वि.सं १८४", "प्रद्युम्न"),
+        ("वि.सं ५२०", "श्री सोमनाथ आत्रेय")
+    ],
+    "timeline_eng":[
+        ("600 BCE", "Atreya"),
+        ("150 BCE", "Charaka"),
+        ("127 AD", "Pradyumna"),
+        ("463 AD", "Shri Somanath Atreya")
+    ],
+}
