e535f0c68f656799c91cd5d4ca4583a6bda7411f
Aashish Poudel <aashishpoudel@Aashishs-MacBook-Air.local>
2025-10-16 17:37:57 -0700

    genealogy timeline

--- genealogy_constants.py
+++ genealogy_constants.py
@@ -5,12 +5,22 @@ TIMELINE_DATA = {
         ("ई.पू ६००", "आत्रेय"),
         ("ई.पू १५०", "चरक"),
         ("वि.सं १८४", "प्रद्युम्न"),
-        ("वि.सं ५२०", "श्री सोमनाथ आत्रेय")
+        ("वि.सं ५२०", "श्री सोमनाथ आत्रेय"),
+        ("वि.सं १२४६", "श्रीवत्सराज", "डोटीका राजा श्री राधामाधवसिंह विष्टले राजकुमारीसँग यिनको विवाह गरी सम्पूर्ण राज्य दान दिएका। जेठी ब्राह्माणीबाट 'सिग्देल' थरका पूर्वज 'श्रीकृष्ण' र कान्छी ब्राह्माणी बाट 'पौड्याल' थरका पूर्वज 'श्रीदेव' भएको।"),
+        ("वि.सं १६८२", "गोपाल", "राजा सिद्धिनरसिह मल्लबाट सिस्नेरीमा विर्ता पाई यही बसोबास गरेका। पुत्रेधिट यज्ञ गराएर पुत्र जन्मेवापत विर्ता पाएको कुरा जनश्रुति र युवराज श्रीनिवासको वि.सं. १६८४ मा जन्म भएअनुसार माथिको समय अनुमानित गरिएको।"),
+        ("वि.सं १७३४", "प्रजापति", "ऑटीघरेका मुल पूर्खा। राजा श्रीनिवासबाट आफ्नै घर जग्गाको साँधमा विर्ता दान मिलेको ताम्रपत्रमा उल्लेख छ।"),
+        ("वि.सं १७३४", "देवाली", "सिस्नेरीमा देवाली गर्न शुरू गरेको मिति।"),
+        ("वि.सं १८८६", "विश्वम्भर", "राजा राजेन्द्र विक्रमको पौडेलहरूको नाममा जारी गरिएको लालमोहरमा उनको नाम उल्लेख भेटिन्छ।"),
     ],
     "timeline_eng":[
         ("600 BCE", "Atreya"),
         ("150 BCE", "Charaka"),
         ("127 AD", "Pradyumna"),
-        ("463 AD", "Shri Somanath Atreya")
+        ("463 AD", "Shri Somanath Atreya"),
+        ("1189 AD", "Shri Batsaraj", "Doti King Shri Radhamadhav Bista married the princess to him and donated the entire Kingdom. From his elder wife, the ancestor of the 'Sigdel' clan ('Shri Krishna') started. Likewise, from younger wife, the ancestor of the 'Poudel' clan ['Shri Dev']"),
+        ("1625 AD", "Gopal", "Received land grant from King Siddhinarsingh Malla in Sisneri and settled here. He supposedly performed Putredhit Yagya, and since prince Srinivas was born in 1627 AD, the above date is estimated"),
+        ("1677 AD", "Prajapati", "The original ancestor of the AatiGhare family. He is mentioned in a King Srinivas' copper-plate inscription that the king granted him land next to his own land"),
+        ("1677 AD", "Dewali", "Sisneri 'Dewali' festival (देववली) start year"),
+        ("1829 AD", "Bishomvar", "His name is mentioned in the red seal issued in the name of the Poudels of King Rajendra Vikram"),
     ],
 }
