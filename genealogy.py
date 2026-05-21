6355b64cf562dae28c56a32085fbec683de97918
Aashish Poudel <aashishpoudel@Aashishs-MacBook-Air.local>
2026-05-21 12:36:26 -0700

    genealogy - somnath to gopal

--- genealogy.py
+++ genealogy.py
@@ -687,7 +687,7 @@ def generate_vertical_ancestral_html(person):
 
     return render_node(person)
 
-def export_ancestral_tree_html(root_person, output_file="before_gopal.html"):
+def export_ancestral_tree_html(root_person, output_file="somnath_to_gopal.html"):
     body_html = generate_vertical_ancestral_html(root_person)
 
     html = f"""
@@ -817,7 +817,7 @@ if __name__ == "__main__":
 
     # export_ancestral_tree_html(
     #     first_person_listed,
-    #     "before_gopal.html"
+    #     "somnath_to_gopal.html"
     # )
 
     #Before Gopal Timeline HTML
@@ -829,7 +829,7 @@ if __name__ == "__main__":
         import sys
 
         gedcom_file = "./data/Somnath_Gopal.ged"
-        output_file = "before_gopal.html"
+        output_file = "somnath_to_gopal.html"
 
         print(f"Parsing {gedcom_file}...")
         parser = GEDCOMParser(gedcom_file)
